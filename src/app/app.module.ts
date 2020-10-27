36d673a6cb5c50c2fb1e5b56cb6d65556672c175
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-10-27 15:45:01 -0500

    feature: fixed http error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
