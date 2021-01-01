5fd2ed1f78b9422e035cb39860e0410eadfe3fc5
Ramon Hubbell <Ramon.hubbell@gmail.com>
2021-01-01 10:12:47 -0600

    feature: set an event to execute onCreateServer

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,19 +1,31 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html', 
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
 
-  constructor() { }
+  constructor( ) { 
+    setTimeout( ( ) => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
-  ngOnInit(): void {
+  ngOnInit( ): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created!';
   }
 
 }
